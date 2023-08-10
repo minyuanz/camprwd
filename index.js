f7f8ae60c813948304e787866b41d7e5b1ba438c
minyuan <a1097562@gmail.com>
2023-08-10 23:30:09 +0800

    modify

---
+++ index.js
@@ -0,0 +1,128 @@
+$(document).ready(function(){
+
+
+//banner輪播
+let divWidth = $(".slider-container").width() / 4;
+let imgCount = $(".slidepic").length;
+let index = 0;
+
+function moveToNext() {
+    if (index < imgCount - 1) {
+        index++;
+    }
+    else if (index == 3) {
+        index = 0;
+        $(".slider-container").css("left", "0");
+    };
+    $(".slider-container").animate({
+        left: index * divWidth * -1
+    });
+};
+
+setInterval(moveToNext, 2000);
+
+
+//按鈕左右
+let currentindex = 0;
+$("#btnleft").click(function () {
+    $("#btnright").removeAttr("disabled");
+    currentindex--;
+    let width = $(".news").width() + 40;
+    // console.log(width);
+    $(".news_group").css("left", `-${width * currentindex}px`);
+    if (currentindex == 0) {
+        $("#btnleft").attr("disabled", "true");
+    }
+
+    // console.log(currentindex);
+});
+
+if (window.innerWidth < 768) {
+    $("#btnright").click(function () {
+        currentindex++;
+        let width = $(".news").width() + 40;
+        $(".news_group").css("left", `-${width * currentindex}px`);
+        if (currentindex === 5) {
+            $("#btnright").attr("disabled", "true");
+        }
+        $("#btnleft").removeAttr("disabled");
+
+        // console.log(currentindex);
+
+    });
+}
+else if (window.innerWidth < 1200) {
+    $("#btnright").click(function () {
+        currentindex++;
+        let width = $(".news").width() + 40;
+        $(".news_group").css("left", `-${width * currentindex}px`);
+        if (currentindex === 4) {
+            $("#btnright").attr("disabled", "true");
+        }
+        $("#btnleft").removeAttr("disabled");
+
+        console.log(currentindex);
+
+    })
+};
+
+//page2top
+$(".page2top").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: 0
+    }, 700)
+
+});
+
+
+//算物件高
+let winHeight = $(window).innerHeight();
+// console.log(winHeight);
+$(window).scroll(function () {
+    let winScrolltop = $(this).scrollTop();
+    // console.log(winScrolltop);
+    let all = winHeight + winScrolltop;
+    // console.log(all);
+    $(".slideIn").each(function () {
+        let slideofftop = $(this).offset().top;
+        // console.log(slideofftop);
+        if (all > slideofftop) {
+            $(this).addClass("active")
+        }
+    })
+
+});
+
+//點擊滑動
+// let isDown = false;
+// let startX;
+// let scrollLeft;
+
+// $(".news_group").mousedown(function (e) {
+//     isDown = true;
+//     console.log(e.pageX);
+//     // startX = e.pageX - this.offsetLeft;
+//     // scrollLeft = this.scrollLeft;
+// });
+
+// $(".news_group").mouseleave(function () {
+//     isDown = false;
+// });
+
+// $(".news_group").mouseup(function () {
+//     isDown = false;
+// });
+
+// $(".news_group").mousemove(function (e) {
+//     if (isDown != true) {
+//         return;
+//     };
+
+//     e.preventDefault;
+//     let x = e.pageX - this.offsetLeft;
+//     let walk = x - startX;
+//     this.scrollLeft = scrollLeft - walk;
+
+// });
+})
